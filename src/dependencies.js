0b1245d479ddd9971cc84b717d640f0ad3761352
Viktor Zhakhalov <zhakhalov@gmail.com>
2016-03-26 00:17:17 +0200

    ngResource added to dependencies

--- src/dependencies.js
+++ src/dependencies.js
@@ -2,11 +2,13 @@ require('angular');
 require('angular-ui-router');
 require('angular-sanitize');
 require('angular-animate');
+require('angular-resource');
 
 require('ionic');
 
 require('lodash');
 
 angular.module('dependencies',[
-  'ionic'
+  'ionic',
+  'ngResource'
 ]);
